76cd7e6406a3f949c7e925b8f6df85237b2c9682
jtompkins415 <john.tompkins415@gmail.com>
2023-01-23 14:23:24 -0800

    create functions for creating star html, deletion html, and function for deleting a story

--- js/stories.js
+++ js/stories.js
@@ -23,8 +23,13 @@ function generateStoryMarkup(story) {
   // console.debug("generateStoryMarkup", story);
 
   const hostName = story.getHostName();
+
+  const showStar = Boolean(currentUser);
+
   return $(`
       <li id="${story.storyId}">
+      ${showDeleteBtn ? getDeleteBtnHTML() : ""}
+      ${showStar ? getStarHTML(story, currentUser) : ""}
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
         </a>
@@ -34,6 +39,26 @@ function generateStoryMarkup(story) {
       </li>
     `);
 }
+
+
+function getDeleteBtnHTML(){
+  return `
+    <span class="trash-can">
+      <i class="fas fa-trash-alt"></i>
+    </span>`;
+}
+
+function getStarHTML(story, user){
+  const isFavorite = user.isFavorite(story);
+  const starType = isFavorite ? "fas" : "far";
+  return `
+    <span class="star">
+      <i class="${starType} fa-star"></i>
+    </span>`;
+}
+
+
+
 
 /** Gets list of stories from server, generates their HTML, and puts on page. */
 
@@ -49,4 +74,58 @@ function putStoriesOnPage() {
   }
 
   $allStoriesList.show();
+}
+
+async function deleteStory(evt){
+  console.debug('deleteStory');
+
+  const $closestLi = $(evt.target).closest("li");
+  const storyId = $closestLi.attr("id");
+
+  await storyList.removeStory(currentUser, storyId);
+
+  await putUserStoriesOnPage();
+}
+
+$ownStories.on("click", ".trash-can", deleteStory);
+
+async function handleStorySubmit(evt){
+  console.debug("handleStorySubmit");
+  evt.preventDefault();
+
+  const title = $("new-story-title").val();
+  const author = $("new-story-author").val();
+  const url = $("new-story-url").val();
+  const username = currentUser.username;
+
+  const formData = {title, author, url, username};
+
+  const story = await storyList.addStory(currentUser, formData);
+
+  const $story = generateStoryMarkup(story);
+  $allStoriesList.prepend($story);
+
+  $newStoryForm.slideUp('slow');
+  $newStoryForm.trigger('reset');
+
+}
+
+$newStoryForm.on("submit", handleStorySubmit);
+
+function putUserStoriesOnPage() {
+  console.debug("putUserStoriesOnPage");
+
+  $ownStories.empty();
+
+  if(currentUser.ownStories.length === 0){
+    $ownStories.append("<h5>No Stories added by user yet!</h5>");
+  } else {
+    for(let story of currentUser.ownStories){
+      let $story = generateStoryMarkup(story, true);
+      $ownStories.append($story)
+    }
+  }
+
+  $ownStories.show();
+
 }
